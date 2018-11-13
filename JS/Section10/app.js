b3a58cc4311ea5b58edbecc233725950b134eac1
Jayfiled <jayfiled@gmail.com>
2018-11-13 12:54:45 +1100

    js revision for ZtM

---
+++ JS/Section10/app.js
@@ -0,0 +1,22 @@
+
+const database = [
+    {
+        username: "jayfiled",
+        password: "allDrainsLeadToTheOcean"
+    }
+];
+
+const newsFeed = [
+    {
+        username: "emily",
+        timeline: "I\'m hungry"
+    },
+    {
+        username: "jason",
+        timeline: "working remotely!"
+    },
+    {
+        username: "megan",
+        timeline: "shopping therapy?!"
+    }
+];
