d511459d145db0f477e67e6bb5a8f21bab472d09
Jayfiled <jayfiled@gmail.com>
2018-11-17 21:58:51 +1100

    finish background generator - DOM practice

--- projects/backgroundGenerator/script.js
+++ projects/backgroundGenerator/script.js
@@ -1,11 +1,14 @@
 const colorLeft = document.querySelector("#grad-left");
 const colorRight = document.querySelector("#grad-right");
 const body = document.querySelector("body");
+const linearGrad = document.querySelector("p");
 
 colorLeft.addEventListener('input', function() {
     body.style.background = "linear-gradient(to left, " + colorLeft.value + ", " + colorRight.value + ")"; 
-
 })
 
+colorRight.addEventListener('input', function() {
+    body.style.background = "linear-gradient(to left, " + colorLeft.value + ", " + colorRight.value + ")"; 
+})
 
-console.log(body.style.background);
+linearGrad.innerHTML = colorRight.value + ", " + colorLeft.value;
