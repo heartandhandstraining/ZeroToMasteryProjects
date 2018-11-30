3877a45326261bc768c51c956391802b8c58d873
Jayfiled <jayfiled@gmail.com>
2018-11-30 21:54:31 +0000

    create card component and robot hash api

---
+++ projects/robotFriends/robofriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+            <div className="bg-light-green dib br3 ma2 grow bw2 shadow-5">
+                <img src="https://robohash.org/set_set4/miaow?200x200" alt="" />
+                <div>
+                    <h2>John Smith</h2>
+                    <p>johnsmith@gmail.com</p>
+                </div>
+            </div>
+    );
+};
+
+export default Card;
