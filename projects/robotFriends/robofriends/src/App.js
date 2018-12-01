39ae61f6b10cf5f05afacc7d50f124ddbad3950d
Jayfiled <jayfiled@gmail.com>
2018-12-01 17:56:39 +0000

    created component tree

---
+++ projects/robotFriends/robofriends/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { cats } from './cats';
+
+const App = () => {
+    return (
+        <div className="tc">
+            <h1>MiaowyFriends</h1>
+            <SearchBox />
+            <CardList cats={cats} />
+        </div>
+    );
+};
+
+export default App;
--- projects/robotFriends/robofriends/src/index.js
+++ projects/robotFriends/robofriends/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+// import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { cats } from './cats';
 
-ReactDOM.render(
-                <div>
-                    <Card id={cats[0].id} name={cats[0].name} email={cats[0].email} />
-                    <Card id={cats[1].id} name={cats[1].name} email={cats[1].email} />
-                    <Card id={cats[2].id} name={cats[2].name} email={cats[2].email} />
-                    <Card id={cats[3].id} name={cats[3].name} email={cats[3].email} />
-                </div>
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
