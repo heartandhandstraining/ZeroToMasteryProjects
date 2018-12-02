2c00561f109d31624899626c17f3f281c4d602e5
Jayfiled <jayfiled@gmail.com>
2018-12-02 18:10:38 +0000

    add some imgages for readme

--- JS/Section 12/exercise5.js
+++ JS/Section 12/exercise5.js
@@ -30,31 +30,50 @@ const array = [
 //Create an array using forEach that has all the usernames with a "!" to each of the usernames
 
 // I want to chain the whole thing..
+  array.forEach((obj) => {
 
-// This should do it, however it doesn't mutate the array array..
-array.forEach((obj) => {
-
-  const split = obj.username.split("");
-  split.unshift("!"); 
-  const join = split.join("");
-  
-  });
+    const split = obj.username.split("");
+    split.unshift("!"); // unshift doesn't return the array with changed values, only the length of the array
+    obj.username = split.join("");
+    
+    });
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
 
+const newArr = array.map(el => {el.username = `?${el.username}`;
+})
 
 //Filter the array to only include users who are on team: red
 
+const filtered = array.filter(el => el.team === "red");
 
 //Find out the total score of all users using reduce
 
-// (1), what is the value of i?
+const totalScore = array.reduce((accum, currentVal) => accum + currentVal.score, 0);
+
+// (1), what is the value of i?         0, 1, 2, 3, 4, 5
 // (2), Make this map function pure:
 const arrayNum = [1, 2, 4, 5, 8, 9];
 const newArray = arrayNum.map((num, i) => {
-	console.log(num, i);
-	alert(num);
 	return num * 2;
 })
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+
+const newArr = array.map(user => {
+
+    return user.items.map(item => {
+
+      return item = `${item}!`
+  
+  })
+})
+
+const newArrWithReduce = array.reduce((acc, eachElement) => {
+      return acc += eachElement;
+}, []);
+
+
+const newArrWithReduce = array.reduce((acc, eachElement) => {
+  acc = [...array];
+}, []);
---
+++ Resources/readme.md
@@ -0,0 +1,18 @@
+
+An image gallery
+https://jayfiled.github.io/ZeroToMasteryProjects/ImageGallery/index.html
+
+A robot build with flexbox
+https://jayfiled.github.io/ZeroToMasteryProjects/Resources/Robot/index.html
+
+A site to generate nice gradient backgrounds
+https://jayfiled.github.io/ZeroToMasteryProjects/projects/backgroundGenerator/index.html
+
+A page for launching a product with a sign up option
+https://jayfiled.github.io/ZeroToMasteryProjects/projects/productLanding/index.html
+
+A shopping list made by manipulating the DOM
+https://jayfiled.github.io/ZeroToMasteryProjects/projects/shoppingList/index.html
+
+A React app that fetches state from a json object and filters the result to show different types of CATS.
+(Todo: find out how to host it on GitHub pages)
