1e196451ff4517174a29aff3bb8e95879ea463c7
Jayfiled <jayfiled@gmail.com>
2018-12-05 19:22:29 +0000

    Updates

---
+++ precache-manifest.890eb59750c77515608baa9296eebe45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "005999f586dac6b95020",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/static/js/runtime~main.005999f5.js"
+  },
+  {
+    "revision": "2e2c10668d1b9582ed07",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/static/js/main.2e2c1066.chunk.js"
+  },
+  {
+    "revision": "68d560afc21ae31f0d51",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/static/js/1.68d560af.chunk.js"
+  },
+  {
+    "revision": "2e2c10668d1b9582ed07",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/static/css/main.c6df5e28.chunk.css"
+  },
+  {
+    "revision": "68d560afc21ae31f0d51",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/static/css/1.a3cedc0e.chunk.css"
+  },
+  {
+    "revision": "05f09a61876bb9178469c9b242cd61d7",
+    "url": "/ZeroToMasteryProjects/projects/robotFriends/index.html"
+  }
+];
