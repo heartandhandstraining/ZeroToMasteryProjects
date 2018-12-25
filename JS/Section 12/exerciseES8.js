770994d77475b2f2ba52c5b1dc4ade20124d44cf
Jayfiled <jayfiled@gmail.com>
2018-12-25 23:06:19 +1100

    ES8 padStart(), End(), Object.entries()  brush up

--- JS/Section 12/exerciseES8.js
+++ JS/Section 12/exerciseES8.js
@@ -2,8 +2,8 @@
 
 // #1) Line up the Turtle and the Rabbit at the start line:
 const startLine = '     ||<- Start line';
-let turtle = '🐢';
-let rabbit = '🐇';
+let turtle = '       🐢';
+let rabbit = '       🐇';
 
 // it should look like this:
 '     ||<- Start line'
@@ -15,11 +15,18 @@ console.log(startLine);
 console.log(turtle);
 console.log(rabbit);
 
+// answer - the turtle is two chars and padStart(9) fills in the space with as many characters inclusive of it's current length
+const startLine = '     ||<- Start line';
+let turtle = '🐢'.padStart(9);
+let rabbit = '🐇'.padStart(9);
+
 
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
-turtle = turtle.trim().padEnd(9, '=');
+turtle = turtle.trim().padEnd(9, '=');  // "🐢======="
 
+// The whitespace is removed with .trim() then for a total of 9 characters including what is already in the let variable
+// replace the white space with 'equals to' operator, or whichever char you enter as the second argument
 
 // #3) Get the below object to go from:
 let obj = {
